b09bc584cd05c136e2c8c3416fedca84d23efd6f
Alex Saladna <asaladna@smu.edu>
2016-05-01 19:36:22 -0500

    comments added

--- servoOut/servoOut.ino
+++ servoOut/servoOut.ino
@@ -7,8 +7,6 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  150
 #define SERVOMAX  600
 
-int test= 9;
-bool flag = true;
 int movement[16];
 int msg[16];
 uint8_t servonum = 0;
@@ -62,27 +60,29 @@ void loop() {
     counter++;
 
   //actual pin commands odering them to move
+
+  //row 1
   pwm.setPWM(0, 0, movement[0]);
   pwm.setPWM(1, 0, movement[1]);
   pwm.setPWM(2, 0, movement[2]);
   pwm.setPWM(3, 0, movement[3]);
 
-//  delay(500);
-  
+
+  //row 2
   pwm.setPWM(4, 0, movement[4]);
   pwm.setPWM(5, 0, movement[5]);
   pwm.setPWM(6, 0, movement[6]);
   pwm.setPWM(7, 0, movement[7]);
 
-//  delay(500);
-  
+
+  //row 3
   pwm.setPWM(8, 0, movement[8]); 
   pwm.setPWM(9, 0, movement[9]);
   pwm.setPWM(10, 0, movement[10]);
   pwm.setPWM(11, 0, movement[11]);
 
-//  delay(500);
-  
+
+  //row 4
   pwm.setPWM(12, 0, movement[12]);
   pwm.setPWM(13, 0, movement[13]);
   pwm.setPWM(14, 0, movement[14]);
